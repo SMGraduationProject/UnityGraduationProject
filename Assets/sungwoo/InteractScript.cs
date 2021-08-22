e29be757d098c72ca7e76e7b6cccceecf5a81242
skwon123 <81675200+skwon123@users.noreply.github.com>
2021-08-22 12:00:04 +0900

    8/22 sw

---
+++ Assets/sungwoo/InteractScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class InteractScript : MonoBehaviour { 
+    public float interactDiastance = 5f; 
+    
+    void Update() { 
+        if (Input.GetKeyDown(KeyCode.E)) {
+            Ray ray = new Ray(transform.position, transform.forward); 
+            RaycastHit hit; 
+            if (Physics.Raycast(ray, out hit, interactDiastance)) { 
+                if (hit.collider.CompareTag("Door")) { 
+                    hit.collider.transform.parent.GetComponent<DoorScript>().ChangeDoorState(); } 
+            } 
+        } 
+    } 
+}
